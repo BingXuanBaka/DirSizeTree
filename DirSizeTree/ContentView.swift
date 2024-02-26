93d763702be8fe92287447aac7f1ca26722e18d6
BingXuanOwO <hxc35459@outlook.com>
2024-02-26 11:15:56 +0800

    chore: init

---
+++ DirSizeTree/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  DirSizeTree
+//
+//  Created by 冰轩 on 2024/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DirSizeTree/DirSizeTreeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DirSizeTreeApp.swift
+//  DirSizeTree
+//
+//  Created by 冰轩 on 2024/2/25.
+//
+
+import SwiftUI
+
+@main
+struct DirSizeTreeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
